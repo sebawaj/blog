fca2348856e58845835fadc2acf44b30a0d367e6
Andy Viera <79466971+andy-viera@users.noreply.github.com>
2023-02-09 15:03:15 -0300

    v0.2

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+require("dotenv").config();
+
 const express = require("express");
-const app = express();
-const mysql = require("mysql2");
-const dotenv = require("dotenv").config();
-const port = process.env.APP_PORT;
 const routes = require("./routes/routes");
+const dbInitialSetup = require("./dbInitialSetup");
+const port = process.env.APP_PORT;
+const app = express();
 
 app.set("view engine", "ejs");
 
 app.use("/", routes);
+
+dbInitialSetup();
 
 app.listen(port, () => {
   console.log(`Listening... http://localhost:${port}`);
