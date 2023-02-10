5460c6018e85ec738b0ed21a3f1a70873f0e2386
Andy Viera <79466971+andy-viera@users.noreply.github.com>
2023-02-10 12:56:42 -0300

    v0.5
    Home ejs

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const dbInitialSetup = require("./dbInitialSetup");
 const port = process.env.APP_PORT;
 const app = express();
 
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
